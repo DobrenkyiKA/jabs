2b9c4e3a041fb4d875ea64f44fb968e40de31c2d
Konstantin Dobrenkii <konstantin_dobrenkii@epam.com>
2025-05-05 19:21:51 +0400

    Initialize the project

---
+++ src/main/java/com/kdob/jabs/JabsApplication.java
@@ -0,0 +1,13 @@
+package com.kdob.jabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdob/jabs/JabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdob.jabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
