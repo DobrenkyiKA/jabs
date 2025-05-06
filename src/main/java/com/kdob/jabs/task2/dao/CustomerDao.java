2635c3fa7e2687b472db625ebe9087a9122bb638
Konstantin Dobrenkii <konstantin_dobrenkii@epam.com>
2025-05-06 14:51:39 +0400

    Task 2.2 add dao
    - add Customer persistence entity

---
+++ src/main/java/com/kdob/jabs/task2/dao/CustomerDao.java
@@ -0,0 +1,61 @@
+package com.kdob.jabs.task2.dao;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class CustomerDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "age")
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
