201ef4b8d14a0e82f8d40d7af825b87ee8bd2e4a
Konstantin Dobrenkii <konstantin_dobrenkii@epam.com>
2025-05-08 18:45:05 +0400

    Convert DTO classes to Java records.

--- src/main/java/com/kdob/jabs/task2/dto/ErrorMessage.java
+++ src/main/java/com/kdob/jabs/task2/dto/ErrorMessage.java
@@ -1,28 +1,4 @@
 package com.kdob.jabs.task2.dto;
 
-public class ErrorMessage {
-    private String message;
-    private String errorCode;
-
-    public ErrorMessage(final String message, final String errorCode) {
-        this.message = message;
-        this.errorCode = errorCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(String errorCode) {
-        this.errorCode = errorCode;
-    }
-
+public record ErrorMessage(String message, String errorCode) {
 }
