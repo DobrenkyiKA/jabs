89cabdbfb1ab52209ee19bec6779fd5b8e5300a1
Konstantin Dobrenkii <konstantin_dobrenkii@epam.com>
2025-05-15 11:30:30 +0400

    Task 3.3 add pre-authorization for customer's endpoints

--- src/main/java/com/kdob/jabs/task2/controller/CustomerController.java
+++ src/main/java/com/kdob/jabs/task2/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.kdob.jabs.task2.controller;
 
 import com.kdob.jabs.task2.dto.*;
 import com.kdob.jabs.task2.facade.CustomerFacade;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,21 +16,25 @@ public class CustomerController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public CreateCustomerResponse createCustomer(@RequestBody final CreateCustomerRequest request) {
         return customerFacade.createCustomer(request);
     }
 
     @GetMapping(path = "/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public GetCustomerResponse getCustomer(@PathVariable final Long id) {
         return customerFacade.getCustomer(id);
     }
 
     @PutMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public UpdateCustomerResponse updateCustomer(@RequestBody final UpdateCustomerRequest request, @PathVariable final Long id) {
         return customerFacade.updateCustomer(request, id);
     }
 
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void deleteCustomer(@PathVariable final Long id) {
         customerFacade.deleteCustomer(id);
     }
